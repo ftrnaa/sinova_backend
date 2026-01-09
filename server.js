d2c43490f07c4e9517ebf9225bf4bf57a2d49c60
Fitri Nabila Aprianti - 3312401012 <fitrinabilaaprianti2006@gmail.com>
2026-01-09 11:28:12 +0700

    Change server port from 5000 to 4001

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // ==============================
 // START SERVER (HANYA SEKALI)
 // ==============================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT} 🚀`);
 });
